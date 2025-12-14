e88624e0a67db24dc15a2e374c5888b39326e426
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-12-14 17:42:00 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /**
+         * You are given an integer array 'height' of length 'n'. There are n vertical
+         * lines drawn such that the two endpoints of the ith line are (i , 0) and (i ,
+         * height[i]).
+         * 
+         * Find two lines that together with the x-axis form a container, such that the
+         * container conatins the most water.
+         * 
+         * Return the maximum amount of water a container can store.
+         * 
+         * Notice that you may ot slant the container.
+         */
+        System.out.println("Hello, World!");
+    }
+}
